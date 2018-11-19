9d08f155c2db77c58f9093640d1d7b8376d13cb8
M-fekry <32997691+M-fekry@users.noreply.github.com>
2018-11-19 23:45:46 +0200

    Task#2

---
+++ Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Campo.Models
+{
+    public class Category
+    {
+        public int categoryID { get; set; }
+        public String categoryName { get; set; }
+    }
+}
