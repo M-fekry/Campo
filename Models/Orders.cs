b2367c52717d7464516bff5b7029903b3958dfda
Feiby19 <44720910+Feiby19@users.noreply.github.com>
2018-11-19 23:31:32 +0200

    Add files via upload

---
+++ Models/Orders.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampProj.Models
+{
+    public class Orders
+    {
+        public int Order_ID { get; set; }
+        public int prod_ID { get; set; }
+        public int order_date { get; set; }
+        public int Quant { get; set; }
+
+
+
+
+    }
+}
