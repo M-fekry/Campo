81159fc5fcd158e98d3c3a4baf51a5f15a8800aa
Rasha-Gamal <44783930+Rasha-Gamal@users.noreply.github.com>
2018-11-24 15:29:08 +0200

    Add files via upload

---
+++ Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Campo.Models
+{
+    public class Category
+    {
+        [key]
+        public int categoryID { get; set; }
+        public String categoryName { get; set; }
+
+        public ICollection<Products> productsList { get; set; }
+
+    }
+}
---
+++ Orders.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampProj.Models
+{
+    public class Orders
+    {
+        [key]
+        public int Order_ID { get; set; }
+        public int prod_ID { get; set; }
+        public int order_date { get; set; }
+        [required]
+        public int Quant { get; set; }
+
+
+        // forgein key cutomer id 
+        public int CustomerID { get; set; }
+        public  Customers _Customers  {get; set; }
+
+
+          //forgein key product id 
+         public int ProductId { get; set; }
+         public  products _Prod { get; set; }
+
+    }
+}
